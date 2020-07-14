547d0f9fa5c2f9f56fcb82efbf0e43e78e64ec79
DANAI <1760785235@qq.com>
2020-07-14 01:33:35 +0000

    用户激活

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
+use Mail;
 
 class UsersController extends Controller
 {
     public function __construct() {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store','index']
+            'except' => ['show', 'create', 'store', 'index', 'confirmEmail']
         ]);
 
         $this->middleware('guest', [
@@ -44,9 +45,15 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
-        Auth::login($user);//用户注册成功后能够自动登录
-        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');//在网页顶部位置显示注册成功的提示信息
-        return redirect()->route('users.show', [$user]);
+        //通过注册邮箱验证:
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
+        return redirect('/');
+
+        //直接注册然后自动登录:
+        // Auth::login($user);//用户注册成功后能够自动登录
+        // session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');//在网页顶部位置显示注册成功的提示信息
+        // return redirect()->route('users.show', [$user]);
     }
 
     public function edit(User $user) {
@@ -78,5 +85,30 @@ class UsersController extends Controller
         $user->delete();
         session()->flash('success', '成功删除用户！');
         return back();
+    }
+
+    protected function sendEmailConfirmationTo($user) {//该方法将用于发送邮件给指定用户
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = '1760785235@qq.com';
+        $name = 'DANAI';
+        $to = $user->email;
+        $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {//??????
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
+    }
+
+    public function confirmEmail($token) {//用于完成用户的激活操作
+        $user = User::where('activation_token', $token)->firstOrFail();//取出第一个用户
+
+        $user->activated = true;//将该用户的激活状态改为true
+        $user->activation_token = null;//激活令牌设置为空
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
     }
 }
